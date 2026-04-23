a3b3815e53e9cd3c333400ee58469e30abe35ef7
PCK216 <PCK216@K216>
2026-04-23 15:23:43 +0300

    LINQ задачи Select, Where, OrderBy, FirstOrDefault, Count, Min, Max

--- EFCodeFirstDb/Program.cs
+++ EFCodeFirstDb/Program.cs
@@ -8,7 +8,8 @@ namespace EFCodeFirstDb
         static void Main(string[] args)
         {
             var options = new DbContextOptionsBuilder<AppDbContext>()
-    .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CodeFirstDb;TrustServerCertificate=False;Integrated Security=True;")
+    .UseSqlServer(
+  "Server=(localdb)\\MSSQLLocalDB;Database=CodeFirstDb;TrustServerCertificate=False;Integrated Security=True;")
     .Options;
 
             using var db = new AppDbContext(options);
@@ -17,21 +18,24 @@ namespace EFCodeFirstDb
 
             //foreach (var product in products)
             //{
-            //    Console.WriteLine(product.Id+". "+product.Name);
+            //    Console.WriteLine(product.Id + ". " + product.Name);
             //}
 
-            //var productsNamesPrice = db.Products.Select(
-            //    p => new
-            //    {
-            //        ProductName = p.Name,
-            //        ProductPrice = p.Price
+            var productsNamesPrice = db.Products.Select(
+                p => new
+                {
+                    ProductName = p.Name,
+                    ProductPrice = p.Price,
+                    PriceWithVat = p.Price * 1.2m
 
-            //    }).ToList();
+                }).ToList();
 
-            //foreach(var product in productsNamesPrice)
-            //{
-            //    Console.WriteLine(product.ProductName+" - "+product.ProductPrice);
-            //}
+            foreach (var product in productsNamesPrice)
+            {
+                Console.WriteLine(product.ProductName + " - " + product.ProductPrice+" price with VAT: "+
+                    product.PriceWithVat);
+            }
+
 
             var productCategory = db.Products.Select(
                 p => new
@@ -45,6 +49,38 @@ namespace EFCodeFirstDb
             {
                 Console.WriteLine(product.ProductName+ " - "+product.Category);
             }
+
+            // Products price > 100
+
+            var productsOver100 = db.Products.Where(p => p.Price>100 ).ToList();
+
+            // Product's names starts with letter L
+
+            var productsStartsWithL = db.Products.Where(p=>p.Name.StartsWith("L")).ToList();
+
+            // OrderBy ASC price
+            var priceOrder = db.Products.OrderByDescending(p => p.Price).Take(3).ToList();
+
+            // Count
+
+            var productsCount = db.Products.Count(p => p.Price > 100);
+
+            // Max and Min price
+
+            var minPrice = db.Products.Min(p => p.Price);
+            var maxPrice = db.Products.Max(p => p.Price);
+
+            // Count products in each category
+            var categoriesProductsCount = db.Categories.Select(c => new
+            {
+                categoryName = c.Name,
+                productsCount = c.Products.Count()
+            }).ToArray();
+
+            //  first product with price over 100
+
+            var firstProduct = db.Products.FirstOrDefault(p => p.Price > 100);
+
 
 
 
